7314fec45b9a956ac3ddfdf370b468f29a6387f1
youseong <hysskm@gmail.com>
2023-11-03 20:55:16 +0900

    딥러닝과 컴퓨터 비전

---
+++ Edge_and_Region/허프변환.py
@@ -0,0 +1,13 @@
+import cv2 as cv 
+
+img = cv.imread("ImageProcessing//apple.jpg")
+
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+apples = cv.HoughCircles(gray,cv.HOUGH_GRADIENT,1,200,param1=100,param2=10,minRadius=50,maxRadius=120)
+
+for i in apples[0]:
+    cv.circle(img,(int(i[0]),int(i[1])),int(i[2]),(255,0,0),2)
+
+cv.imshow('Apple detection',img)
+cv.waitKey()
+cv.destroyAllWindows()
