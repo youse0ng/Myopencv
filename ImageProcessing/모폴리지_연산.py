7c6ae8df6b7680ce7153292981d059f4c2eb077b
youseong hwang <hysskm@gmail.com>
2023-11-21 13:14:04 +0900

    고전 컴퓨터 알고리즘

--- ImageProcessing/모폴리지_연산.py
+++ ImageProcessing/모폴리지_연산.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 img = cv.imread("ImageProcessing\JohnHancocksSignature.png",cv.IMREAD_UNCHANGED)
 print(img.shape)
-t,bin_img=cv.threshold(img[:,:,3],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+t,bin_img=cv.threshold(img[:,:,3],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU) # 오츄 알고리즘을 통한 Thershold 이진화 
 plt.imshow(bin_img,cmap='gray'),plt.xticks([]),plt.yticks([])
 plt.show()
 
