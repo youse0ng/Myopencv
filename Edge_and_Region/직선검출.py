6b2d28676e667f92ba6e291a3c760d0406cdb19d
youseong hwang <hysskm@gmail.com>
2023-11-21 13:12:11 +0900

    고전 컴퓨터 알고리즘

--- Edge_and_Region/직선검출.py
+++ Edge_and_Region/직선검출.py
@@ -3,7 +3,7 @@ import numpy as np
 
 img = cv.imread("Edge_and_Region\soccer.jpg")
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-canny = cv.Canny(gray,70,170)
+canny = cv.Canny(gray,70,170) # 캐니 알고리즘 (NMS:비최대 억제) # 미분을 통한 선검출
 
 contour,hierarchy = cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
 print(len(contour))
